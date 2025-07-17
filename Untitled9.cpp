ada30550664ee728deec546b3061b5f3f9ac934f
Rittik10 <itsmerittikdas@gmail.com>
2025-07-17 23:04:10 +0530

    Update Untitled9.cpp

--- Untitled9.cpp
+++ Untitled9.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for (i = 1; i <= 10; ++i)
         printf("%d x %d = %d\n", num, i, num * i);
-//for loop use kri jikunu no r 10 leke * table bonai dibo parisu
+
     return 0;
 }
 
