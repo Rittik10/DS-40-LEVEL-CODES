d13a603ec4bf051ec0defd1bc7cba84ab170eef9
Rittik10 <itsmerittikdas@gmail.com>
2025-07-17 22:56:48 +0530

    Update question 3.cpp

--- question 3.cpp
+++ question 3.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#define PI 3.14159  // pi r value tu define kri disu
+#define PI 3.14159  // pi value is defined first
 
 int main() {
     float radius, area;
 
     printf("Enter radius: ");
-    scanf("%f", &radius);  // radius tu loi loisu
+    scanf("%f", &radius);  
 
-    area = PI * radius * radius;  // formula tu
+    area = PI * radius * radius;  
     printf("Area = %.2f\n", area);
 
     return 0;
